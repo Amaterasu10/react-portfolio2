4e9b430b7d1601f279f61182f97750abe75ca9ad
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-08 15:11:05 +0800

    added routes directory and test routes

---
+++ src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import Card from "../components/Card"
+export default function Home() {
+  return (
+    <div className="grid">
+      <Card img="https://via.placeholder.com/300x250"/>
+      <Card img="https://via.placeholder.com/300x250"/>
+      <Card img="https://via.placeholder.com/300x250"/>
+      <Card img="https://via.placeholder.com/300x250"/>
+    </div>
+  )
+}
