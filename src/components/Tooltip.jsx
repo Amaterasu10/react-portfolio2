c4b7f7b5e90930c042f86f53d570ea5dbafdce50
Hinrich <johnhinrichggalindo@gmail.com>
2022-04-09 14:57:06 +0800

    improved the tooltip so that its basing its style from its classnames and props

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,10 +1,10 @@
 
-export default function Tooltip({tip, children}) {
+export default function Tooltip({className="", tip="tip", children, pos="right"}) {
   return (
-    <div className="tooltip">
+    <div className={"tooltip " + className}>
         {children}
-        <span className="tooltip__text">
-            {tip}
+        <span className={"tooltip__text " + pos}>
+          {tip}
         </span>
     </div>
   )
